0399ae4a44c65e5f1381bd945859fd9fd43c8314
zlobereth <dabane55@gmail.com>
2026-09-16 14:35:33 +0300

    Add files via upload

---
+++ test_items.py
@@ -0,0 +1,16 @@
+import time
+
+from selenium.webdriver.common.by import By
+
+
+def test_guest_should_see_add_to_basket_button(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    time.sleep(30)
+
+    button = browser.find_element(
+        By.CSS_SELECTOR,
+        ".btn-add-to-basket"
+    )
+
+    assert button.is_displayed(), "Add to basket button is not displayed"
